da1cea70a4f196ecc7bf232f42785b8fbe91a6b1
Saif-Rahman-801 <saifurs102@gmail.com>
2024-11-24 11:18:22 +0600

    Starter file for TS_express projects

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import express from "express";
+import cors from "cors"
+const app = express();
+
+// perser
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+// app.
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+
+// getting-started.js
+import mongoose from "mongoose";
+import config from "./app/config";
+
+// main().catch(err => console.log(err));
+async function main() {
+  try {
+    await mongoose.connect(config.databaseUrl as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
